db1979204c231d39304beceaa5cb9d42f4f6e2eb
Julia797 <almatezza@inbox.ru>
2023-02-28 23:16:42 +0300

    попап и жс

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*let formElement = document.querySelector('.form');
-
-      formElement.addEventListener('submit', function () {
-          console.log('Форма отправлена');
-      })
-      ;*/
-
       let popup = document.querySelector('.popup');
       let btnEdit = document.querySelector('.btn-edit');
       let btnClose = document.querySelector('.btn-close');
@@ -18,6 +11,28 @@
       btnClose.addEventListener('click', function () {
         popup.classList.remove('popup_opened');
       });
+
+      let popupInput = document.querySelector('.popup__input');
+      let nameInput = document.querySelector('.popup__name');
+      let infoInput = document.querySelector('.popup__info');
+      let profileTitle = document.querySelector('.profile__title');
+      let profileSubtitle = document.querySelector('.profile__subtitle');
+      let btnSave = document.querySelector('.btn-save');
+
+      btnSave.addEventListener('click', function handleFormSubmit (evt) {
+        evt.preventDefault();
+        profileTitle.textContent = nameInput.value;
+        profileSubtitle.textContent = infoInput.value;
+        popup.classList.remove('popup_opened');
+      });
+
+
+
+
+
+
+
+/*console.log(nameInput.value);*/
 
 
 
