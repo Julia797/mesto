df9818605477f89757e67ec51ad4f6d1cec5ff10
Julia797 <almatezza@inbox.ru>
2023-03-01 22:30:07 +0300

    бэм и баги

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
-      let popup = document.querySelector('.popup');
-      let btnEdit = document.querySelector('.btn-edit');
-      let btnClose = document.querySelector('.btn-close');
+let popup = document.querySelector('.popup');
+let btnEdit = document.querySelector('.profile__btn-edit');
+let btnClose = document.querySelector('.popup__btn-close');
 
+btnEdit.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+});
 
+btnClose.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
+});
 
-      btnEdit.addEventListener('click', function () {
-        popup.classList.add('popup_opened');
-      });
+let form = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name');
+let infoInput = document.querySelector('.popup__info');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let btnSave = document.querySelector('.popup__btn-save');
 
-      btnClose.addEventListener('click', function () {
-        popup.classList.remove('popup_opened');
-      });
+btnSave.addEventListener('click', function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = infoInput.value;
+  popup.classList.remove('popup_opened');
+});
 
-      let popupInput = document.querySelector('.popup__input');
-      let nameInput = document.querySelector('.popup__name');
-      let infoInput = document.querySelector('.popup__info');
-      let profileTitle = document.querySelector('.profile__title');
-      let profileSubtitle = document.querySelector('.profile__subtitle');
-      let btnSave = document.querySelector('.btn-save');
-
-      btnSave.addEventListener('click', function handleFormSubmit (evt) {
-        evt.preventDefault();
-        profileTitle.textContent = nameInput.value;
-        profileSubtitle.textContent = infoInput.value;
-        popup.classList.remove('popup_opened');
-      });
-
-
-
-
-
-
-
-/*console.log(nameInput.value);*/
-
-
-
+form.addEventListener('submit', handleFormSubmit);
 
